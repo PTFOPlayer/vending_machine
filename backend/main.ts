dd51ce23e040b5fc8ca561504f325e50183f9f0f
PTFOPlayer <patryk041103@gmail.com>
2023-04-01 21:10:43 +0200

    added backend to project

---
+++ backend/main.ts
@@ -0,0 +1,10 @@
+const express = require("express")
+const port = 3000
+
+const app = express()
+
+app.get("/", (req: any,res: { send: (arg0: string) => void }) => {
+    res.send("lorem")
+})
+
+app.listen(port, () => console.log(`listening on: ${port}`))
