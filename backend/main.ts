dadac20acb5e19868469cfa8088c263332e1f1d5
DScraftPL <xxxwiacekxxx@gmail.com>
2023-04-01 22:25:01 +0200

    poczatek backendu - polaczenie z baza danych

--- backend/main.ts
+++ backend/main.ts
@@ -1,5 +1,15 @@
 import express from "express"
-const port = 3000
+import mongoose from "mongoose"
+
+const port = 3000;
+const mongoString = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000";
+
+//utworzenie bazy danych, łącząc się z sewerem lokalnym
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => console.log(error))
+database.once('connected', () => console.log("Database Connected"))
 
 const app = express()
 
