e6c9ad009ff1dc4b667e9a3ec62049d26347fdb1
karo-w <karolinawolodko@wp.pl>
2023-04-15 23:14:54 +0200

    Machine-Appearance

---
+++ src/MachineAppearance/MachineApp.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import './machineApp.scss'
+//import Machine from "../Machine/Machine";
+
+export default function View(){
+    return(<>
+    <div className="box">
+        <div className="interior"></div>
+        <div className="push">
+            <h1>PUSH</h1>
+        </div>
+        <div className="window"></div>
+        <div className="coins">
+            <p>coins</p>
+        </div>
+        
+    </div>
+    </>
+    )
+}
+
+
+
+
